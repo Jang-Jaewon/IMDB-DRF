b59f860eadf2650e139dc402758b70f6c81ec365
Jang-Jaewon <jewon119@gmail.com>
2022-02-19 15:45:21 +0900

    Add: Validators Test

--- contents/api/serializers.py
+++ contents/api/serializers.py
@@ -3,10 +3,14 @@ from rest_framework import serializers
 from contents.models import Movie
 
 
+def description_length(value):
+    if len(value) < 10:
+        raise serializers.ValidationError("Description is too short!")
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(validators=[description_length])
     active = serializers.BooleanField()
     
     def create(self, validated_data):
