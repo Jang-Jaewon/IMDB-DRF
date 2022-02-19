8abbfc1ad8adec04aef150ec2f42f01658449eaf
Jang-Jaewon <jewon119@gmail.com>
2022-02-19 15:37:16 +0900

    Add: Field-level validation Test

--- contents/api/serializers.py
+++ contents/api/serializers.py
@@ -18,3 +18,8 @@ class MovieSerializer(serializers.Serializer):
         instance.active      = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Name is too short!")
+        return value
