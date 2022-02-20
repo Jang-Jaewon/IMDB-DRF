eef6321205ccbefac1159311cdd2e7b5e5f09da7
Jang-Jaewon <jewon119@gmail.com>
2022-02-20 14:39:29 +0900

    Modify: Chage serializer for nested data

--- contents/api/serializers.py
+++ contents/api/serializers.py
@@ -3,15 +3,17 @@ from rest_framework import serializers
 from contents.models import StreamPlatform, Content
 
 
-class StreamPlatformSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model  = StreamPlatform
-        fields = "__all__"
-
-
 class ContentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model  = Content
         fields = "__all__"
+        
+        
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    contentlist = ContentSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model  = StreamPlatform
+        fields = "__all__"
+
