67da164dd25597968db7b64d4ce0a86253110f82
Jang-Jaewon <jewon119@gmail.com>
2022-02-20 18:11:21 +0900

    Fix: Change Url Structure

--- contents/api/urls.py
+++ contents/api/urls.py
@@ -5,9 +5,9 @@ from .views import StreamPlatformAPIView, StreamPlatformDetailAPIView, ContentLi
 
 urlpatterns = [
     path('stream/', StreamPlatformAPIView.as_view(), name='streamplatform'),
-    path('stream/<int:pk>', StreamPlatformDetailAPIView.as_view(), name='streamplatform-detail'),
+    path('stream/<int:pk>/', StreamPlatformDetailAPIView.as_view(), name='streamplatform-detail'),
     path('list/', ContentListAPIView.as_view(), name='content-list'),
     path('<int:pk>/', ContentDetailAPIView.as_view(), name='content-detail'),
-    path('review/', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    path('stream/<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
