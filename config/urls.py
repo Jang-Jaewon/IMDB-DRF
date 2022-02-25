c611a447a0639c770a74d0fc99b0c65dec30e365
Jang-Jaewon <jewon119@gmail.com>
2022-02-25 15:40:36 +0900

    Add: Implement basic Login, Logout functions provided by DRF.

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('content/', include('contents.api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
