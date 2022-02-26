e6375b76b79a1b1b074a839ef8a45e2f86ee2774
Jang-Jaewon <jewon119@gmail.com>
2022-02-26 16:02:39 +0900

    Add: When Logout, delete Token.

--- users/api/urls.py
+++ users/api/urls.py
@@ -2,10 +2,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from django.urls import path
 
-from users.api.views import registration_view
+from users.api.views import registration_view, logout_view
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('register/', registration_view, name='register'),
 ]
--- users/api/views.py
+++ users/api/views.py
@@ -1,9 +1,17 @@
 from rest_framework.decorators       import api_view
 from rest_framework.response         import Response
 from rest_framework.authtoken.models import Token
+from rest_framework                  import status
 
 from users.api.serializers import RegistrationSerializer
 
+
+@api_view(['POST',])
+def logout_view(request):
+    
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(['POST',])
 def registration_view(request):
