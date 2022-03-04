cce082e0b0518cb16a31cfb8a09d1fdccac07893
Jang-Jaewon <jewon119@gmail.com>
2022-03-04 14:14:00 +0900

    Add: Brownable APIRender and JSONRender in Default Settings

--- config/settings.py
+++ config/settings.py
@@ -148,6 +148,10 @@ REST_FRAMEWORK = {
     },
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'PAGE_SIZE': 5,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
 }
 
 # SIMPLE_JWT = {
