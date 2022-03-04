94ac1b3a04d299eca2cb579a0483900def19bdca
Jang-Jaewon <jewon119@gmail.com>
2022-03-04 13:52:39 +0900

    Modify: Change LimitOffsetPagination

--- contents/api/pagination.py
+++ contents/api/pagination.py
@@ -1,9 +1,15 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
-class ContentListPagination(PageNumberPagination):
-    page_size             = 5
-    page_query_param      = 'p'
-    page_size_query_param = 'size'
-    max_page_size         = 50
-    last_page_strings     = 'end'
+# class ContentListPagination(PageNumberPagination):
+#     page_size             = 5
+#     page_query_param      = 'p'
+#     page_size_query_param = 'size'
+#     max_page_size         = 50
+#     last_page_strings     = 'end'
+
+class ContentListPagination(LimitOffsetPagination):
+    default_limit      = 5
+    max_limit          = 10
+    limit_query_param  = 'limit'
+    offset_query_param = 'start'
